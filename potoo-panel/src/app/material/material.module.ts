7fbaa1579e6f6d2b23fcfdaa0427287d2610979a
aymerichenouille <aymerichenouille@penguin>
2020-02-18 18:42:30 +0100

    feat: images pop
    - display all potoo images
    - run animation before all images

--- potoo-panel/src/app/material/material.module.ts
+++ potoo-panel/src/app/material/material.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
-
+const modules = [
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ modules ],
+  exports: [ modules ]
 })
 export class MaterialModule { }
--- potoo-panel/src/app/pages/potoos/models/potoo.model.ts
+++ potoo-panel/src/app/pages/potoos/models/potoo.model.ts
@@ -3,4 +3,5 @@ export interface Potoo {
     image: string;
     name: string;
     description: string;
+    date: Date;
 }
