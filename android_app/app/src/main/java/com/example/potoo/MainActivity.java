8399f3d43d432b5603f7b4efdc3c6457f6bbd1f2
guibresz <50353802+guibresz@users.noreply.github.com>
2020-02-18 22:08:04 +0100

    Affiche potoo
    changé de APK sur lollipop
    ajout d'un bouton qui change le background

--- android_app/app/src/main/java/com/example/potoo/MainActivity.java
+++ android_app/app/src/main/java/com/example/potoo/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.potoo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bouton_potoo = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bouton_potoo = findViewById(R.id.button_potoo);
+        bouton_potoo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RelativeLayout layout = (RelativeLayout)findViewById(R.id.layout_accueil);
+
+                layout.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.potoo));
+
+            }
+        });
+
     }
 }
